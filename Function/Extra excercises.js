b1ba5582fc0ba25887f4638caac120dc27d05122
nguyendaitriet <triet9510@gmail.com>
2022-03-10 14:22:57 +0700

    Done

---
+++ Function/Extra excercises.js
@@ -0,0 +1,45 @@
+function createArray(size, min = 0, max = 10) {
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+        arr[i] = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    return arr;
+}
+// console.log(createArray(9));
+
+function checkPrimeNumberInArray(arr) {
+    for (let index in arr) {
+        let check = true;
+        for (let k = 2; k < arr[index]; k++) {
+            if (arr[index] % k == 0) {
+                check = false;
+                break;
+            }
+        }
+        if (check == true) {
+            return true;
+        }
+    }
+    return false;
+}
+// console.log(checkPrimeNumberInArray([4,6,8,10,12,3]));
+
+function calculateSumAll(arr) {
+    let sum = 0;
+    for (let value of arr) {
+        sum+=value;
+    }
+    return sum;
+}
+// console.log(calculateSum([2,3,4]));
+
+function countEvenNumber(arr) {
+    let count = 0;
+    for (let number of arr) {
+        if (number%2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+// console.log(countEvenNumber([2,44,10,5,17,19,6,-15]));
